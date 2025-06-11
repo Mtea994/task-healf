ea4d49538749db97f3b8bba17efef0f6ac65d07d
mtea994 <engg_imtinan@yahoo.com>
2025-06-11 14:31:38 +0500

    initial-commit: project initialized

---
+++ src/components/Layout/Navigation.tsx
@@ -0,0 +1,25 @@
+import { Input } from "@/components/ui/input";
+import { ShoppingBag } from "lucide-react";
+
+function LayoutNavigation() {
+  return (
+    <header className="bg-accent-foreground flex items-center h-16 w-full">
+      <div className="flex w-full max-w-[1440px] items-center justify-between mx-auto">
+        <div>
+          <h4 className="text-background font-semibold text-2xl">Logo</h4>
+        </div>
+        <div>
+          <Input
+            className="bg-background w-2xl"
+            placeholder="Search Products"
+          />
+        </div>
+        <div>
+          <ShoppingBag color="white" size={24} />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default LayoutNavigation;
